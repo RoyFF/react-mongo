cae17518174271cccedec06c0572fe3e018f12e6
ff_roy_solera <roy@firstfactory.com>
2017-11-22 16:30:35 -0600

    form

--- dev/js/containers/user-list.js
+++ dev/js/containers/user-list.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import * as actions from '../actions/index';
-import { Table, Button} from 'react-bootstrap';
+import { Table, Button, Form, FormGroup, FormControl, Col, ControlLabel, Checkbox} from 'react-bootstrap';
 
 
 class UserList extends Component {
@@ -24,7 +24,63 @@ class UserList extends Component {
     render() {
         return (
             <div>
-                <Button bsStyle="primary" onClick={() => this.props.addUser(user)}>New User</Button>
+                <Form horizontal>
+                    <FormGroup controlId="formHorizontalName">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Name
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="text" placeholder="Name" value="asdasd"/>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="formHorizontalLast">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Last
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="text" placeholder="Last" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="formHorizontalAge">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Age
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="number" placeholder="Age" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="formHorizontalDescription">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Description
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="text" placeholder="description" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="formHorizontalThumbnail">
+                        <Col componentClass={ControlLabel} sm={2}>
+                        Thumbnail
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="text" placeholder="thumbnail" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="formHorizontalEmail">
+                        <Col componentClass={ControlLabel} sm={2}>
+                            Email
+                        </Col>
+                        <Col sm={10}>
+                            <FormControl type="email" placeholder="Email" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col smOffset={2} sm={10}>
+                            <Button bsStyle="primary" onClick={() => this.props.addUser(user)}>
+                            Add User
+                            </Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
                 <Table responsive striped bordered condensed hover>
                     <thead>
                         <tr>
@@ -37,7 +93,7 @@ class UserList extends Component {
                     <tbody>
                     {this.renderList()}
                     </tbody>
-            </Table>
+                </Table>
             </div>
         );
     }
